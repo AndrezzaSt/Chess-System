84c4b1dda8c069b7a295a28b7327816c6a2c058b
AndrezzaSt <andrezza.sturm@hotmail.com>
2021-06-21 14:23:02 -0300

    Handling exceptions and clearing screen

---
+++ src/application/Program.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package application;
+
+import boardgame.Board;
+import boardgame.Position;
+import chess.ChessException;
+import chess.ChessPiece;
+import chess.ChessPosition;
+import chess.Chessmatch;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author andrezzas
+ */
+public class Program {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Chessmatch chessMatch = new Chessmatch();
+        while (true) {
+            try {
+                UI.clearScreen();
+                UI.printBoard(chessMatch.getPieces());
+                System.out.println();
+                System.out.println("Source: ");
+                ChessPosition source = UI.readChessPosition(sc);
+                System.out.println();
+                System.out.println("Target: ");
+                ChessPosition target = UI.readChessPosition(sc);
+
+                ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+            }
+            catch(ChessException e){
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
+            catch(InputMismatchException e){
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
+        }
+    }
+}
--- src/application/UI.java
+++ src/application/UI.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package chess.system;
+package application;
 
 import chess.ChessPiece;
 import chess.ChessPosition;
@@ -36,6 +36,12 @@ public class UI {
     public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
     public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
     public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+
+    // https://stackoverflow.com/questions/2979383/java-clear-the-console
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
 
     public static ChessPosition readChessPosition(Scanner sc) {
         try {
