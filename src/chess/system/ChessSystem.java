ba5cd6766be0b659a9ce02302e7812f85e24625b
AndrezzaSt <andrezza.sturm@hotmail.com>
2021-06-21 13:56:33 -0300

    Moving Pieces

--- src/chess/system/ChessSystem.java
+++ src/chess/system/ChessSystem.java
@@ -7,7 +7,10 @@ package chess.system;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessPiece;
+import chess.ChessPosition;
 import chess.Chessmatch;
+import java.util.Scanner;
 
 /**
  *
@@ -19,10 +22,18 @@ public class ChessSystem {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); 
         Chessmatch chessMatch = new Chessmatch();
-        UI.printBoard(chessMatch.getPieces()); 
-                
-                
+        while (true) {
+            UI.printBoard(chessMatch.getPieces());
+            System.out.println();
+            System.out.println("Source: ");
+            ChessPosition source = UI.readChessPosition(sc);
+            System.out.println();
+            System.out.println("Target: ");
+            ChessPosition target = UI.readChessPosition(sc);
+
+            ChessPiece capturedPiece = chessMatch.performChessMove(source, target); 
+        }
     }
-    
 }
