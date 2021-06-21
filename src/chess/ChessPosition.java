049b62839d1931643ad1b1856d1fd106320cd153
AndrezzaSt <andrezza.sturm@hotmail.com>
2021-06-21 12:51:04 -0300

    ChessException and ChessPosition

---
+++ src/chess/ChessPosition.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chess;
+
+import boardgame.Position;
+
+/**
+ *
+ * @author andrezzas
+ */
+public class ChessPosition {
+    private char column;
+    private int row;
+
+    public ChessPosition(char column, int row) {
+        if(column < 'a' || column > 'h' || row < 1 || row > 8){
+            throw new ChessException("Erro instantiating ChessPosition. Valid Values are from a1 to h8."); 
+        }
+        this.column = column;
+        this.row = row;
+    }
+
+    public char getColumn() {
+        return column;
+    }
+
+
+    public int getRow() {
+        return row;
+    }
+
+    protected Position toPosition(){
+        return new Position(8 - row, column - 'a' );
+    }
+    
+    protected static ChessPosition fromPosition(Position position){
+        return new ChessPosition((char)('a' - position.getColumn()), 8 - position.getRow());
+    }
+    
+    @Override
+    public String toString(){
+        return "" + column + row;
+    }
+}
