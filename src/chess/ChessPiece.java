394f0115469c241347dc839c247c85761a484e44
AndrezzaSt <andrezza.sturm@hotmail.com>
2021-06-22 13:08:52 -0300

    Possible moves of a piece

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -12,7 +12,7 @@ import boardgame.Piece;
  *
  * @author andrezzas
  */
-public class ChessPiece extends Piece{
+public abstract class ChessPiece extends Piece{
       private Color color; 
 
     public ChessPiece(Board board, Color color ) {
