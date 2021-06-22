60458e9e3f28c76129b8d03acfee04a88297e4b1
AndrezzaSt <andrezza.sturm@hotmail.com>
2021-06-22 14:07:14 -0300

    Printing possible moves

--- src/application/Program.java
+++ src/application/Program.java
@@ -33,17 +33,20 @@ public class Program {
                 System.out.println();
                 System.out.println("Source: ");
                 ChessPosition source = UI.readChessPosition(sc);
+
+                boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+                UI.clearScreen();
+                UI.printBoard(chessMatch.getPieces(), possibleMoves);
+
                 System.out.println();
                 System.out.println("Target: ");
                 ChessPosition target = UI.readChessPosition(sc);
 
                 ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-            }
-            catch(ChessException e){
+            } catch (ChessException e) {
                 System.out.println(e.getMessage());
                 sc.nextLine();
-            }
-            catch(InputMismatchException e){
+            } catch (InputMismatchException e) {
                 System.out.println(e.getMessage());
                 sc.nextLine();
             }
--- src/chess/Chessmatch.java
+++ src/chess/Chessmatch.java
@@ -34,6 +34,14 @@ public class Chessmatch {
         return mat;
     }
 
+    
+    public boolean[][] possibleMoves(ChessPosition sourcePosition){
+        Position position = sourcePosition.toPosition();
+        validateSourcePosition(position); 
+        return board.piece(position).possibleMoves(); 
+    }
+    
+    
     public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
         Position source = sourcePosition.toPosition();
         Position target = targetPosition.toPosition();
